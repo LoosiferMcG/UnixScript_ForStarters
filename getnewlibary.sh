fd8e06530889f63bf616879743f70247874c2987
Jon "The Nice Guy" Spriggs <jon@sprig.gs>
2019-10-18 10:05:19 +0100

    A couple of fixes
    * Added more "help"
    * Added a lost curled brace
    * Removed the comments at the top.

--- getnewlibary.sh
+++ getnewlibary.sh
@@ -1,17 +1,9 @@
-##getnewlibary.sh
 #! /bin/bash
 library="$HOME/Upstreams/"
 function help() {
   echo "Syntax: $0 'filename'"
+  echo "This script will replace the file you provide with an exactly-the-same named version in $library"
   exit 1
-}
-function help_file() {
-  echo "Missing file! (You may have passed a directory?)"
-  help
-}
-function help_notavailable() {
-  echo "This file does not exist. Skipping."
-  exit 0
 }
 if [ ! -f "$1" ]; then help_file; fi
 filename="$(basename "$1")"
