0e4ce19b3842176741f0bf78f595259873078964
Mayuresh K <mskadu@users.noreply.github.com>
2019-10-18 13:20:19 +0100

    Shellcheck fixes
    Fixing issues identified by ShellCheck
    
    * SC1128: The shebang must be on the first line. Delete blanks and move comments.
    * SC2088: Tilde does not expand in quotes. Use $HOME.
    * SC2086: Double quote to prevent globbing and word splitting.
    
    Added comments header including usage example. Please correct as necessary ;-)

--- RehouseMedia.sh
+++ RehouseMedia.sh
@@ -1,22 +1,30 @@
-#RehouseMedia.sh
 #!/bin/bash
 
-#Setting the debug options
+# RehouseMedia.sh
+# Shell script to move media files to a different folder organized by year and month
+#
+# Usage:
+#     ./RehouseMedia.sh {Media file pattern} {target owner}
+# Example:
+#     ./RehouseMedia.sh *.jpg LoosiferMcG
+#
+
+# Setting the debug options
 debug_echo() {
   if [ -n "$DEBUG" ]; then
     echo "$1"
   fi
 }
-#Error out if missing source files
+# Error out if missing source files
 if [ -z "$1" ] || [ ! -f "$1" ]; then
   echo "Missing source file"
   exit 1
 fi
-#Error out if missing a target owner
+# Error out if missing a target owner
 if [ -z "$2" ]; then
   echo "Missing target owner"
   exit 1
-elif [ -f "~/Dropbox/Stored Photos for ${2}" ]; then
+elif [ -f "$HOME/Dropbox/Stored Photos for ${2}" ]; then
   TARGET_PHOTO="Stored Photos for ${2}"
   TARGET_VIDEO="Stored Videos for ${2}"
   TARGET_AUDIO="Stored Audio for ${2}"
@@ -26,7 +34,7 @@ else
 fi
 
 # Based on https://stackoverflow.com/a/47706445 and https://stackoverflow.com/questions/47706415/how-does-this-bash-script-work/47706445#comment82373061_47706445
-#Sorting the files into Audio, photo or video based on their file extensions.
+# Sorting the files into Audio, photo or video based on their file extensions.
 if [[ "${1: -4}" =~ ^.([Jj][Pp][Gg])$ ]] || [[ "${1: -5}" =~ ^.([Jj][Pp][Ee][Gg])$ ]] || [[ "${1: -4}" =~ ^.([Pp][Nn][Gg])$ ]] || [[ "${1: -4}" =~ ^.([Gg][Ii][Ff])$ ]]; then
   TARGET="$TARGET_PHOTO"
 elif [[ "${1: -4}" =~ ^.([Mm][Pp]4)$ ]] || [[ "${1: -4}" =~ ^.(3[Gg][Pp])$ ]] || [[ "${1: -4}" =~ ^.([Aa][Vv][Ii])$ ]] || [[ "${1: -4}" =~ ^.([Mm]4[Vv])$ ]]; then
@@ -38,14 +46,14 @@ else
   exit 1
 fi
 
-#Setting the varaiables of the source yr and month
-SOURCE_YEAR="$(echo $1 | sed -r 's/^[^0-9]*([0-9]{4})-?[0-9]{2}.*/\1/')"
-SOURCE_MONTH="$(echo $1 | sed -r 's/^[^0-9]*[0-9]{4}-?([0-9]{2}).*/\1/')"
+# Setting the varaiables of the source yr and month
+SOURCE_YEAR="$(echo "$1" | sed -r 's/^[^0-9]*([0-9]{4})-?[0-9]{2}.*/\1/')"
+SOURCE_MONTH="$(echo "$1" | sed -r 's/^[^0-9]*[0-9]{4}-?([0-9]{2}).*/\1/')"
 
-#Perform final sort of the files into Dropbox folder based on the month/year and whether audio, photos or video files
+# Perform final sort of the files into Dropbox folder based on the month/year and whether audio, photos or video files
 # N.B. this doesn't have to be a dropbox folder, can be anything you chose it to be e.g. Amazon Photos, Amazon Drive.
 debug_echo "Moving $1 to ~/Dropbox/$TARGET/$SOURCE_YEAR/$SOURCE_YEAR-$SOURCE_MONTH/"
-#Make directories as needed per year and month
+# Make directories as needed per year and month
 mkdir -p "$HOME/Dropbox/$TARGET/$SOURCE_YEAR/$SOURCE_YEAR-$SOURCE_MONTH/"
 if [ -n "$DEBUG" ]; then
   mv -v "$1" "$HOME/Dropbox/$TARGET/$SOURCE_YEAR/$SOURCE_YEAR-$SOURCE_MONTH/"
