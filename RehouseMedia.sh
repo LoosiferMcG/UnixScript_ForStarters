5a985d79c94df7ae0a6c7661570ed7a9e1ebe135
LoosiferMcG <56579975+LoosiferMcG@users.noreply.github.com>
2019-10-18 10:47:27 +0100

    Added comments to the operations
    Have added comments to the different operations being performed in the script, so don't have work it all out at a later date :)

--- RehouseMedia.sh
+++ RehouseMedia.sh
@@ -1,6 +1,7 @@
 #RehouseMedia.sh
 #!/bin/bash
 
+#Setting the debug options
 debug_echo() {
   if [ -n "$DEBUG" ]; then
     echo "$1"
@@ -24,6 +25,7 @@ else
 fi
 
 # Based on https://stackoverflow.com/a/47706445 and https://stackoverflow.com/questions/47706415/how-does-this-bash-script-work/47706445#comment82373061_47706445
+#Sorting the files into Audio, photo or video based on their file extensions.
 if [[ "${1: -4}" =~ ^.([Jj][Pp][Gg])$ ]] || [[ "${1: -5}" =~ ^.([Jj][Pp][Ee][Gg])$ ]] || [[ "${1: -4}" =~ ^.([Pp][Nn][Gg])$ ]] || [[ "${1: -4}" =~ ^.([Gg][Ii][Ff])$ ]]; then
   TARGET="$TARGET_PHOTO"
 elif [[ "${1: -4}" =~ ^.([Mm][Pp]4)$ ]] || [[ "${1: -4}" =~ ^.(3[Gg][Pp])$ ]] || [[ "${1: -4}" =~ ^.([Aa][Vv][Ii])$ ]] || [[ "${1: -4}" =~ ^.([Mm]4[Vv])$ ]]; then
@@ -35,9 +37,12 @@ else
   exit 1
 fi
 
+#Setting the varaiables of the source yr and month
 SOURCE_YEAR="$(echo $1 | sed -r 's/^[^0-9]*([0-9]{4})-?[0-9]{2}.*/\1/')"
 SOURCE_MONTH="$(echo $1 | sed -r 's/^[^0-9]*[0-9]{4}-?([0-9]{2}).*/\1/')"
 
+#Perform final sort of the files into Dropbox folder based on the month/year and whether audio, photos or video files
+# N.B. this doesn't have to be a dropbox folder, can be anything you chose it to be e.g. Amazon Photos, Amazon Drive.
 debug_echo "Moving $1 to ~/Dropbox/$TARGET/$SOURCE_YEAR/$SOURCE_YEAR-$SOURCE_MONTH/"
 mkdir -p "$HOME/Dropbox/$TARGET/$SOURCE_YEAR/$SOURCE_YEAR-$SOURCE_MONTH/"
 if [ -n "$DEBUG" ]; then
