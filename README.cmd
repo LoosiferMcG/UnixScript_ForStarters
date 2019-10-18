8ca32f2c7aa0fbf359c684547d9137ab7341838d
LoosiferMcG <56579975+LoosiferMcG@users.noreply.github.com>
2019-10-18 10:00:01 +0100

    Update acknowledgements
    Updated thanks to Jon

--- README.cmd
+++ README.cmd
@@ -1,5 +1,6 @@
+# Using this respository to host Shell files that have been written to help automate things in mostly Ubuntu cos that's what we use.
+# Special thanks to @JonTheNiceGuy for helping set this up with me, as until now never used Github as a contributer.
+
 #  Hacktoberfest
-
 I'm finally learning to do this stuff!!!
-
 I'm really doing it!
